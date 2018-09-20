4ea40bf2ec94bdec1d89061130159381788a9e1f
Christian-Nguyen-01 <christian.nguyen88@gmail.com>
2018-09-20 15:53:42 -0700

    exposed authorization header in cors initializer file to allow front end to read the header

--- db/schema.rb
+++ db/schema.rb
@@ -15,18 +15,16 @@ ActiveRecord::Schema.define(version: 2018_09_19_175339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "jti"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["jti"], name: "index_users_on_jti", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table "listings", force: :cascade do |t|
+    t.string "streetAddress", default: "", null: false
+    t.string "city", default: "", null: false
+    t.string "postalCode", default: "", null: false
+    t.string "state", default: "", null: false
+    t.string "country", default: "", null: false
+    t.string "buildingManager.name", null: false
+    t.string "buildingManager.phoneNumber", null: false
+    t.string "buildingManager.contactHours", null: false
+
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+listing_attributes = [
+  {
+    streetAddress:"3465 Nere Dr.",
+    city:"New York",
+    postalCode:"12345",
+    state:"NY",
+    country:"USA",
+    buildingManager:{
+      name:"Kristaps Porzingis",
+      phoneNumber:"123-456-6654",
+      contactHours:"Never"
+    }
+  },
+  {
+    streetAddress:"12654 Tired Blvd.",
+    city:"Sleepy Town",
+    postalCode:"12121",
+    state:"CC",
+    country:"Morphia",
+    buildingManager:{
+      name:"Christian Slumber",
+      phoneNumber:"111-222-7zzz",
+      contactHours:"11:00 - 11:01 AM"
+    }
+  }
+]
+
+listing_attributes.each do |attributes|
+  Listing.create(attributes)
+end
